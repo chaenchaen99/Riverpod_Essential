96a71bdf4222dd3a0dd6bc0ed1f7aaef44eddddf
chaeyeon <chaenjung.99@gmail.com>
2024-08-06 14:41:05 +0900

    freezed 실습

---
+++ riverpod_freezed/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_freezed/pages/mutable_person_page.dart';
+import 'package:riverpod_freezed/pages/person_page.dart';
+import 'package:riverpod_freezed/widgets/custom_button.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Freezed Data Class',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(20),
+          shrinkWrap: true,
+          children: const [
+            CustomButton(
+              title: 'Person',
+              child: PersonPage(),
+            ),
+            CustomButton(
+              title: 'Mutable Person',
+              child: MutablePersonPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ riverpod_freezed/lib/pages/person_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PersonPage extends StatelessWidget {
+  const PersonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Person'),
+      ),
+    );
+  }
+}
