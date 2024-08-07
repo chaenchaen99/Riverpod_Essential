6a09c260a6613acbfac25bd29cb152af5f463adb
chaeyeon <chaenjung.99@gmail.com>
2024-08-07 14:22:56 +0900

    family modifier & audoDispose modifier 실습

--- provider/lib/pages/family/family_page.dart
+++ provider/lib/pages/family/family_page.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/pages/family/family_provider.dart';
 
-class FamilyPage extends StatelessWidget {
+class FamilyPage extends ConsumerWidget {
   const FamilyPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final helloChaen = ref.watch(familyHelloProvider('Chaen'));
+    final helloJane = ref.watch(familyHelloProvider('Jane'));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('FamilyProvider'),
       ),
-      body: const Center(
-        child: Text('FamilyProvider'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              helloChaen,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              helloJane,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ],
+        ),
       ),
     );
   }
