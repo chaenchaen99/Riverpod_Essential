2dc8437baaf21f941c13c69b7d045c1547f53860
chaeyeon <chaenjung.99@gmail.com>
2024-08-12 18:50:44 +0900

    activity 데이터 통신 enum 상태 프로바이더 구현

---
+++ notifier_provider/lib/pages/enum_activity/enum_activity_provider.dart
@@ -0,0 +1,35 @@
+import 'package:notifier_provider/models/activity.dart';
+import 'package:notifier_provider/pages/enum_activity/enum_activity_state.dart';
+import 'package:notifier_provider/providers/dio_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'enum_activity_provider.g.dart';
+
+@riverpod
+class EnumActivity extends _$EnumActivity {
+  @override
+  EnumActivityState build() {
+    ref.onDispose(() {
+      print('enumActivityProvider disposed');
+    });
+    return EnumActivityState.initial();
+  }
+
+  Future<void> fetchActivity(String activityType) async {
+    state = state.copyWith(status: ActivityStaus.loading);
+
+    try {
+      final response = await ref.read(dioProvider).get("?type=$activityType");
+      final activity = Activity.fromJson(response.data);
+      state = state.copyWith(
+        status: ActivityStaus.success,
+        activity: activity,
+      );
+    } catch (e) {
+      state = state.copyWith(
+        status: ActivityStaus.failure,
+        error: e.toString(),
+      );
+    }
+  }
+}
