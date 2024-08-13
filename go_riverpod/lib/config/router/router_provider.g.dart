a5de68cd2905f49decfb78a69b73f44b86cd2e4d
chaeyeon <chaenjung.99@gmail.com>
2024-08-13 12:36:55 +0900

    go_router statefulShellRoute기능 구현

---
+++ go_riverpod/lib/config/router/router_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$routeHash() => r'e22c7a9154e0fe1c729da9e72a3bb94e3bfe0cb6';
+
+/// See also [route].
+@ProviderFor(route)
+final routeProvider = AutoDisposeProvider<GoRouter>.internal(
+  route,
+  name: r'routeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RouteRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
