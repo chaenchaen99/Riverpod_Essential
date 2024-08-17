6ca27751ad2071f68a86751963ee621aaa0088fe
chaeyeon <chaenjung.99@gmail.com>
2024-08-17 19:13:39 +0900

    todo 관련 providers 생성

---
+++ todo_riverpod_sync/lib/pages/providers/filtered_todos/filtered_todos_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'filtered_todos_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$filtedTodosHash() => r'1fe5f5ccd2f4121d48e02185e134123a874198b8';
+
+/// See also [filtedTodos].
+@ProviderFor(filtedTodos)
+final filtedTodosProvider = AutoDisposeProvider<List<Todo>>.internal(
+  filtedTodos,
+  name: r'filtedTodosProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$filtedTodosHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FiltedTodosRef = AutoDisposeProviderRef<List<Todo>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
