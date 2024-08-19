b28cc88b74086ab475fbcf37dad4bcebd520fc7a
chaeyeon <chaenjung.99@gmail.com>
2024-08-20 08:22:29 +0900

    searchTodo & filterTodo 구현

--- todo_riverpod_sync/lib/pages/todos_page.dart
+++ todo_riverpod_sync/lib/pages/todos_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todo_riverpod_sync/pages/widgets/new_todo.dart';
+import 'package:todo_riverpod_sync/pages/widgets/search_todo.dart';
 
+import 'widgets/filter_todo.dart';
 import 'widgets/todo_header.dart';
 
 class TodosPage extends StatelessWidget {
@@ -16,6 +18,10 @@ class TodosPage extends StatelessWidget {
             children: [
               TodoHeader(),
               NewTodo(),
+              SizedBox(height: 20),
+              SearchTodo(),
+              SizedBox(height: 10),
+              FilterTodo(),
             ],
           ),
         ),
